ff0eb7a1513f9b1b9dbda6acced257684a1d79d5
Kovács Dorina <kovacsdorina2@gmail.com>
2023-02-27 09:54:06 +0100

    mobil app kezdés számláló

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { TouchableHighlight } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
+ 
+  const [countnum, setCountnum] = useState(0);
+  function increment(){
+    let actualCount = countnum;
+    let newCount = ++actualCount;
+    setCountnum(newCount);
+    console.log(newCount);
+  }
+  function resetCounter(){
+    setCountnum(0);
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Számol</Text>
+      <Button title = 'számol' onPress={increment}/>
       <StatusBar style="auto" />
+      <TouchableHighlight  style={styles.button1}><Text style={styles.text}>Számol</Text></TouchableHighlight>
     </View>
+  
   );
 }
 
@@ -17,4 +35,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button1: {
+    backgroundColor: '#4A90E2',
+    padding: '10px',
+    width:'50%',
+    borderRadius: '10px',
+    marginTop: "10px",
+  },
+  text:{
+     color: '#FFFFFF',
+     fontWeight: "600",
+     textAlign: 'center'
+  }
+
 });
+
